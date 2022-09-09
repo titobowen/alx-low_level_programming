c8a58ba82e1e877568387433e4cab02a0575ba65
basit <basitolatunbosun@gmail.com>
2022-09-09 15:03:36 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+ *  prints out the letters in lowercase 
+ */
+int main(void)
+{
+	char n = 'a';
+	for (n='a';n<='z';n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return(0);
+}
+
