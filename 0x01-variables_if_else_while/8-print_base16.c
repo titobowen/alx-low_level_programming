aa5a753f594fd0e5952cfdd203561c33d42c4a60
basit <basitolatunbosun@gmail.com>
2022-09-09 16:53:06 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*base 16 lowercase*/
+int main(void)
+{
+	char a;
+	char b = '0';
+
+	for (b = '0'; b < '10'; b++);
+	{
+		putchar(b);
+	}
+	for (a = 'a'; a <= 'f'; a++);
+	{
+		putchar(a)
+	}
+	putchar('\n')
+	return (0)
+}
