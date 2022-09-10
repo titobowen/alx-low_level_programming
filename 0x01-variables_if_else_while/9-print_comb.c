1e9a42d2ae2eec99c50dca42f60d481f413b4ccc
basit <basitolatunbosun@gmail.com>
2022-09-09 17:34:12 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <studio.h>
+/**
+ * main - Entry point
+ * Description: prints numbers
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		putchar(a);
+		if (a != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+		return (0);
+}
