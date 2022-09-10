2b173716e7283c4d93f5735c6c3efcdd30d11511
basit <basitolatunbosun@gmail.com>
2022-09-10 07:58:00 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints three different characters
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a, b, c;
+	for (a = 0; a <= 9; a++)
+	{
+		if (a != b && a!= c)
+			putchar(a);
+	}
+	for (b = 0; b <= 9; b++)
+	{
+		if (b != a && b!= c)
+			putchar(b);
+	}
+	for (c = 0; c <= 9; c++)
+	{
+		if (c != a && c != b)
+			putchar(c);
+	}
+	putchar(',');
+	putchar(' ');
+	return (0);
+}
