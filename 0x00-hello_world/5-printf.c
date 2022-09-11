d34f5158edefbb4cb15e9934dc64c98cca53630d
basit <basitolatunbosun@gmail.com>
2022-09-11 06:29:21 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - main block
- * Return : 0
+ * main - Entry point
+ * Description: prints text
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
