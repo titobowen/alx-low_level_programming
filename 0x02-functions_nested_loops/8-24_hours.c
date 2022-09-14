9f27ba5601928556a3e486a4c2d60d1cedf7160e
basit <basitolatunbosun@gmail.com>
2022-09-14 14:20:54 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,16 +6,26 @@
  */
 void jack_bauer(void)
 {
-	int hour, mins;
+	int hour= 0;
+	int mins = 0;
+	int h_r;
+	int m_r;
 
 	for (hour = 0; hour <= 23; hour++)
 	{
 		for (mins = 0; mins <= 59; mins++)
 		{
-			
+			h_r = hour % 10;
+			m_r = mins % 10;
+			_putchar(hour / 10 + '0');
+			_putchar(h_r + '0');
+			_putchar(':');
+			_putchar(mins / 10 + '0');
+			_putchar(m_r + '0');
+			minutes++;
+			_putchar('\n');
 		}
+		hour++;
+		mins = 0;
 	}
-	_putchar(hour);
-	_putchar(':');
-	_putchar(mins);
 }
