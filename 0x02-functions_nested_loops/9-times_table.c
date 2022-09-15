055ab0776fcf214f8f51a1c5d791a8b4065eacab
basit <basitolatunbosun@gmail.com>
2022-09-14 21:29:09 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,4 +6,24 @@
  */
 void times_table(void)
 {
-	
+	int row;
+	int column;
+	int result;
+
+	while (row = 0;)
+	{
+		row <= 9;
+		row++;
+	while (column = 0;)
+	{
+		column <= 9;
+		result = row * column;
+		_putchar(result);
+		if (row <= 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	}
+}
