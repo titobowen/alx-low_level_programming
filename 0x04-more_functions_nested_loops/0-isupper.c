fcb024b5534e00876fd45fd1d39a894c5a43725a
basit <basitolatunbosun@gmail.com>
2022-09-15 16:18:31 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - prints an uppercase letter
+ * @c: uppercase variable
+ * Description: prints uppercasde letter
+ * Return: 1 if uppercase
+ */
+int _isupper(int c)
+{
+	if ((c >= A) && (c <= Z))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+
+#endif
