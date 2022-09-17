002e84252ead6fc220b062a5a1183e0303be900c
basit <basitolatunbosun@gmail.com>
2022-09-17 03:52:00 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,11 +6,11 @@
  */
 void print_most_numbers(void)
 {
-	int a = 0;
+	int a = '0';
 
-	while (a <= 9)
+	while (a <= '9')
 	{
-		if (a != 2 && a != 4)
+		if (a != '2' && a != '4')
 		{
 			_putchar(a + '0');
 		}
