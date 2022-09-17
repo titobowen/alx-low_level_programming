eb332dfb965f136957cfbd0c943954410bf9fabd
basit <basitolatunbosun@gmail.com>
2022-09-17 04:51:00 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,19 +7,19 @@
 void more_numbers(void)
 {
 	char a, c;
-	int b = 1;
+	int b = 0;
        
-	while (b <= 10)
+	while (b < 10)
 	{
 		for (a = 0; a <= 14; a++)
 		{
-			a = c;
+			c = a;
 			if (a > 9)
 			{
 				_putchar('1');
 				c = a % 10;
-				_putchar(c + '0');
 			}
+			_putchar(c + '0');
 		}
 		_putchar('\n');
 		b++;
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * print_line - draws straight line
+ * @n: variable
+ * Description: prints a straight line
+ * Return: returns true print line
+ */
+
