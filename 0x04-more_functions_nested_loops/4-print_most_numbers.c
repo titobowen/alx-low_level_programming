b5d74eb881409304d83c793e1e81893959743e64
basit <basitolatunbosun@gmail.com>
2022-09-17 04:02:13 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,13 +6,13 @@
  */
 void print_most_numbers(void)
 {
-	int a = '0';
+	int a = 0;
 
-	while (a <= '9')
+	while (a <= 9)
 	{
-		if (a != '2' && a != '4')
+		if (a != 2 && a != 4)
 		{
-			_putchar(a + '0');
+			_putchar(a + 0);
 		}
 		_putchar('\n');
 		a++;
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,12 +8,19 @@ void more_numbers(void)
 {
 	int a;
 	int b;
+	int c;
 
 	for (b = 1; b <= 10; b++)
 	{
 		for (a = 0; a <= 14; a++)
 		{
-			_putchar(a + '0');
+			a = c;
+			if (a > 9)
+			{
+				_putchar(1);
+				c = a % 10;
+				_putchar(c + '0');
+			}
 		}
 		_putchar('\n');
 	}
