fcff08f9223afb9933d4431f675cd5e699e865a7
basit <basitolatunbosun@gmail.com>
2022-09-16 21:30:39 -0700

    undefined reference to `_putchar

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -10,7 +10,7 @@ void print_numbers(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		_putchar(a);
+		putchar(a);
 	}
-	_putchar('\n');
+	putchar('\n');
 }
--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include "unistd.h"
 /**
  * _putchar - print char c
