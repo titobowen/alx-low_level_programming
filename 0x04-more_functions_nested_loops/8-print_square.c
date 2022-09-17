4242c90329bc02376e45fb885f1e9875b9868876
basit <basitolatunbosun@gmail.com>
2022-09-17 11:01:52 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_square - print square
+ * @size: size variable
+ * Description: print a square
+ * Return: Always 0 (success)
+ */
+void print_square(int size)
+{
+	int a = 0;
+	int b = 0;
+
+	while (a < size && size > 0)
+	{
+		while (b < size && size > 0)
+		{
+			_putchar('#');
+			b++;
+		}
+		_putchar('\n');
+		a++;
+	}
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+}
