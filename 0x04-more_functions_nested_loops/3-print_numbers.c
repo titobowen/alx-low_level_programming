1b7d9302237b1abc32d4b98c2382d94e106768ab
basit <basitolatunbosun@gmail.com>
2022-09-16 20:16:08 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers
+ * Description: prints numbers
+ * Return: Always 0 (success)
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar(a);
+		_putchar('\n');
+	}
+}
