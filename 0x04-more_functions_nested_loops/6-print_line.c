50310d1f02520a6f51657eae3a6592a44d7dbdf9
basit <basitolatunbosun@gmail.com>
2022-09-17 05:08:01 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,4 +5,20 @@
  * Description: prints a straight line
  * Return: returns true print line
  */
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		if (n <= 0)
+		{
+		_putchar('\n');
+		}
+		else
+		{
+			_putchar('_');
+			_putchar('\n');
+		}
+	}
+}
+
 
