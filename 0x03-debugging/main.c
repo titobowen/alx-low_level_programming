6a6c51aa465dc32c7e9fb07d1083181897fc5824
basit <basitolatunbosun@gmail.com>
2022-09-18 09:56:09 -0700

    main.h

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Description: retains zero
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
