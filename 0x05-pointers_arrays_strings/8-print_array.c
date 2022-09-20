d0e03f8a86fef6ddc4d09224e8c601066eb9aa75
basit <basitolatunbosun@gmail.com>
2022-09-19 18:46:35 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,13 +13,9 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		if (n > 0)
+		if (i < n -1 )
 		{
 			printf(", ");
-		}
-		else if (a[i] == '\0')
-		{
-			printf("\n");
 		}
 	}
 	printf("\n");
