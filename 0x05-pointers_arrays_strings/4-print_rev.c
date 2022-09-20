5d9126bbfdff5051bcb1b1cf1f811b082759bd50
basit <basitolatunbosun@gmail.com>
2022-09-19 17:31:08 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - prints reverse strings
+ * @s: string to be printed
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	int len;
+
+	for (len = 0; s[len] != "\0"; len++)
+	{
+	}
+	for (i = len -1; i >= 0; i--)
+	{
+		printf("%d", s[i]);
+	}
+}
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rev_string - string in reverse
+ * @s: string to be printed
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int len, i, j;
+	char v1, v2;
+
+	for (len = 0; s[len] != '\0'; len++
+	{
+	}
+	i = len - 1;
+	j = 0;
+	while (i > j)
+	{
+		v1 = s[i];
+		v2 = s[j];
+		s[j] = v1;
+		s[i] = v2;
+		i--;
+		j++;
+	}
+}
+
+
