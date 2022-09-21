02037faf8d4abfab6a54583dff7ed3a05d02aeec
basit <basitolatunbosun@gmail.com>
2022-09-21 13:30:34 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,10 +14,12 @@ char *_strncpy(char *dest, char *src, int n)
 	while (a < n && src[a] != '\0')
 	{
 		dest[a] = src[a];
+		a++;
 	}
 	while (a < n)
 	{
 		dest[a] = '\0';
+		a++;
 	}
 	return (dest);
 }
