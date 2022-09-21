0c11cba014d80641a515c72272ab388f1dc4daf1
basit <basitolatunbosun@gmail.com>
2022-09-21 12:42:58 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strncpy - copies string
+ * @src: first
+ * @dest: second
+ * @n: number
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a;
+
+	a = 0;
+	while (a < n && src[a] != '\0')
+	{
+		dest[a] = src[a];
+	}
+	while (a < n)
+	{
+		dest[a] = '\0';
+	}
+	return (dest);
+}
