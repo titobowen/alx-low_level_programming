cd99848e0746db9f08232d9689332a920501c94d
basit <basitolatunbosun@gmail.com>
2022-09-22 11:38:40 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,3 +1,19 @@
 #include "main.h"
 /**
- *
+ * string_toupper - changes lowercase to uppercase
+ * @str: string variable
+ * Return: string
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
