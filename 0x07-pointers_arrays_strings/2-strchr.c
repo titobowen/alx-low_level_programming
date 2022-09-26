83fe93b2fe5bd16ae83ef6309b7e36a359fd55d7
basit <basitolatunbosun@gmail.com>
2022-09-26 12:32:35 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *_strchr - licates character
+ * @s: string variable
+ * @c: char to find
+ * Return: s
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s);
+		}
+		else
+		{
+			return ('\0');
+		}
+	}
+}
