c1361ce31301396e357c3e4fdff983fb4408b33e
basit <basitolatunbosun@gmail.com>
2022-09-30 15:52:14 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * @argc: int
+ * @argv: array
+ * Return: 0 always
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
