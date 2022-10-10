cbfb582faeaac74996312fcdc3e686889307db03
basit <basitolatunbosun@gmail.com>
2022-10-10 18:37:38 +0100

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef DOD_H
 #define DOG_H
-
 /**
  * struct dog - struture definition of dog
  * @name: name character string
@@ -12,11 +11,11 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
-#endif	
+#endif
