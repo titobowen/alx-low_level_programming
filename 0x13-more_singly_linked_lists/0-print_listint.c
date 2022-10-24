ca4d99abad91ff0ac8a1d6304e79b41559088336
basit <basitolatunbosun@gmail.com>
2022-10-25 00:17:58 +0100

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,10 +1,12 @@
 #include "lists.h"
 #include <stdio.h>
+
 /**
  * print_listint - Print elements of a singly linked list.
  * @h: Pointer to a list.
  * Return: Integer.
  **/
+
 size_t print_listint(const listint_t *h)
 {
 	const listint_t *tp;
