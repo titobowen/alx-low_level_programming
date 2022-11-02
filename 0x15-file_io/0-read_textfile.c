4b239cf19238cd4c7b4ee92c575c68e3ceeacfa2
basit <basitolatunbosun@gmail.com>
2022-11-02 17:01:32 +0100

    0x15-file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,7 +10,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd;
 	int i, y;
 	char *buf;
-if (!filename)
+    
+    if (!filename)
 	return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
